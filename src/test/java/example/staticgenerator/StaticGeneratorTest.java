89c52431b8d82db386561082a8345c0ae372c281
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-10 23:07:47 +0200

    Add test setup for static generator

--- src/test/java/example/staticgenerator/StaticGeneratorTest.java
+++ src/test/java/example/staticgenerator/StaticGeneratorTest.java
@@ -1,4 +1,19 @@
 package example.staticgenerator;
 
-public class StaticGenerator {
+import com.example.Book;
+import com.example.staticgenerator.templates.entities.IterableRouteEntity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StaticGeneratorTest {
+
+    private static Map<String, List<? extends IterableRouteEntity>> loadDummyData() {
+        Map<String, List<? extends IterableRouteEntity>> entityMap = new HashMap<>();
+        List<Book> books = List.of(new Book(1, "Book One", "Author One", "Summary"),
+                new Book(2, "Book Two", "Author Two", "Summary"));
+        entityMap.put("Books", books);
+        return entityMap;
+    }
 }
