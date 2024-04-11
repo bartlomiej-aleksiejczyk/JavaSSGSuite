7c4ae92f81a43093a9a08274da4f1d6fa7f9f231
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2024-04-11 23:18:55 +0200

    Alter test populate method

--- src/test/java/example/staticgenerator/StaticGeneratorTest.java
+++ src/test/java/example/staticgenerator/StaticGeneratorTest.java
@@ -1,7 +1,6 @@
 package example.staticgenerator;
 
-import com.example.Book;
-import com.example.staticgenerator.templates.entities.IterableRouteEntity;
+import com.example.staticgenerator.templates.entities.IterablePageData;
 
 import java.util.HashMap;
 import java.util.List;
@@ -9,8 +8,8 @@ import java.util.Map;
 
 public class StaticGeneratorTest {
 
-    private static Map<String, List<? extends IterableRouteEntity>> loadDummyData() {
-        Map<String, List<? extends IterableRouteEntity>> entityMap = new HashMap<>();
+    private static Map<String, List<? extends IterablePageData>> loadDummyData() {
+        Map<String, List<? extends IterablePageData>> entityMap = new HashMap<>();
         List<Book> books = List.of(new Book(1, "Book One", "Author One", "Summary"),
                 new Book(2, "Book Two", "Author Two", "Summary"));
         entityMap.put("Books", books);
